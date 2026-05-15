819465d0d67081465b9735ebea78b90a66e3d4d9
da21nny <edgarv21.ev@gmail.com>
2026-05-15 17:17:15 -0300

    Añadir middleware para sanitización de XSS

---
+++ src/middlewares/xssMiddleware.js
@@ -0,0 +1,24 @@
+const escapeHTML = (str) => {
+    return str.replace(/[&<>'"]/g, 
+        tag => ({
+            '&': '&amp;',
+            '<': '&lt;',
+            '>': '&gt;',
+            "'": '&#39;',
+            '"': '&quot;'
+        }[tag] || tag)
+    );
+};
+
+const xssSanitizer = (req, res, next) => {
+    if (req.body) {
+        for (let key in req.body) {
+            if (typeof req.body[key] === 'string') {
+                req.body[key] = escapeHTML(req.body[key]);
+            }
+        }
+    }
+    next();
+};
+
+module.exports = { xssSanitizer };
